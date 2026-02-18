c5e9db18db0890134ff3ee2e81673a2d6d06727e
ounben <48635796+ounben@users.noreply.github.com>
2026-02-19 00:24:14 +0100

    Refactor game data retrieval and update logic

--- lichess_game_to_db.py
+++ lichess_game_to_db.py
@@ -7,7 +7,15 @@ DB_CONFIG = "host=postgres_db dbname=fishnet_stats user=fish_admin password=pass
 LICHESS_TOKEN = "lip_bK..."
 HEADERS = {
     "Authorization": f"Bearer {LICHESS_TOKEN}",
-    "Accept": "application/json"
+    "Accept": "application/json"    
+}
+
+QUERY_PARAMS = {
+    "moves": "false",
+    "clocks": "false",
+    "evals": "false",
+    "accuracy": "true",
+    "division": "false"
 }
 
 def update_metrics_full():
@@ -15,33 +23,35 @@ def update_metrics_full():
         with psycopg.connect(DB_CONFIG) as conn:
             with conn.cursor() as cur:
 
-                # cur.execute("SELECT batch_id FROM metrics WHERE url IS NULL;")
-                # rows = cur.fetchall()
-                cur.execute("SELECT batch_id FROM metrics WHERE url IS NULL ORDER BY time DESC LIMIT 1000;")
+                cur.execute("SELECT batch_id FROM metrics WHERE opening_eco IS NULL ORDER BY time DESC LIMIT 1000;")
                 rows = cur.fetchall()
                 
                 for row in rows:
                     batch_id = row[0]
 
-                    # 
                     if not batch_id or str(batch_id).strip() == "":
                         continue
 
-                    url_api = f"https://lichess.org/api/game/{batch_id}"
+                    url_api = f"https://lichess.org/game/export/{batch_id}"
                     
                     try:
-                        response = requests.get(url_api, headers=HEADERS, timeout=10)
+                        response = requests.get(url_api, headers=HEADERS, params=QUERY_PARAMS, timeout=10)
                         
                         if response.status_code == 200:
                             data = response.json()
                             
-                            
+                         
                             clock = data.get("clock", {})
                             players = data.get("players", {})
                             white = players.get("white", {})
                             black = players.get("black", {})
+                            white_user = white.get("user", {})
+                            black_user = black.get("user", {})
+                            white_analysis = white.get("analysis", {})
+                            black_analysis = black.get("analysis", {})
+                            opening = data.get("opening", {})
 
-                            params = (
+                            db_params = (
                                 data.get("rated"),
                                 data.get("variant"),        #game_variant
                                 data.get("speed"),
@@ -53,16 +63,28 @@ def update_metrics_full():
                                 data.get("status"),         #game_status
                                 clock.get("initial"),
                                 clock.get("increment"),
-                                clock.get("totalTime"),
-                                white.get("userId"),
+                                clock.get("totalTime"),                                
+                                white_user.get("id"),
                                 white.get("rating"),
-                                white.get("ratingDiff"),  
-                                black.get("userId"),
+                                white.get("ratingDiff"),  # 
+                                black_user.get("id"),
                                 black.get("rating"),
                                 black.get("ratingDiff"),
                                 data.get("winner"),
                                 data.get("url"),
-                                batch_id
+                                white_analysis.get("inaccuracy"),
+                                white_analysis.get("mistake"),
+                                white_analysis.get("blunder"),
+                                white_analysis.get("acpl"),
+                                white_analysis.get("accuracy"),
+                                black_analysis.get("inaccuracy"),
+                                black_analysis.get("mistake"),
+                                black_analysis.get("blunder"),
+                                black_analysis.get("acpl"),
+                                black_analysis.get("accuracy"),
+                                opening.get("eco"),
+                                opening.get("name"),
+                                batch_id # 
                             )
 
                             cur.execute("""
@@ -86,19 +108,31 @@ def update_metrics_full():
                                     black_rating = %s,
                                     black_rating_diff = %s,
                                     winner = %s,
-                                    url = %s
+                                    url = %s,
+                                    white_inaccuracy = %s,
+                                    white_mistake = %s,
+                                    white_blunder = %s,
+                                    white_acpl = %s,
+                                    white_accuracy = %s,
+                                    black_inaccuracy = %s,
+                                    black_mistake = %s,
+                                    black_blunder = %s,
+                                    black_acpl = %s,
+                                    black_accuracy = %s,
+                                    opening_eco = %s,
+                                    opening_name = %s
                                 WHERE batch_id = %s
-                            """, params)
+                            """, db_params)
                             
                             conn.commit()
-                            print(f"Update abgeschlossen: {batch_id}")
+                            print(f" Update (a): {batch_id} ")
 
                         elif response.status_code == 429:
                             print("Rate Limit erreicht. Pause 60s.")
-                            time.sleep(120)
+                            time.sleep(60)
                         
                         elif response.status_code == 404:
-                            print(f"Game {batch_id} existiert nicht (404).")
+                            print(f"Game {batch_id} existiert nicht (404). Markiere in DB.")
                         
                             cur.execute("""
                                 UPDATE metrics 
@@ -107,12 +141,11 @@ def update_metrics_full():
                                 WHERE batch_id = %s
                             """, (batch_id,))
                         conn.commit()
-                        # pause
                         # time.sleep(0.05)
-                        time.sleep(1.0)
+                        time.sleep(0.2)
 
                     except Exception as e:
-                        print(f"Fehler  Game {batch_id}: {e}")
+                        print(f"Fehler bei Game {batch_id}: {e}")
 
     except Exception as e:
         print(f"Datenbankfehler: {e}")
